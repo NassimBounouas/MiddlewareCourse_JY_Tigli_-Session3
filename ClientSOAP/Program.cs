950e4f961428051283b3812568a502cf78731b81
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2018-03-22 10:20:41 +0100

    Part1 and Part2 TD : Adress / binding

---
+++ ClientSOAP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ClientSOAP.MathsService;
+
+namespace ClientSOAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client 1");
+            MathsOperationsClient client = new MathsOperationsClient("MathsOperations");
+            Console.WriteLine(client.Add(100, 101));
+            Console.WriteLine(client.Multiply(10, 15));
+
+            Console.WriteLine("Client 2");
+            MathsOperationsClient client2 = new MathsOperationsClient("MathsBis");
+            Console.WriteLine(client2.Add(100, 101));
+            Console.WriteLine(client2.Multiply(10, 15));
+
+            Console.WriteLine("Client 3");
+            MathsOperationsClient client3 = new MathsOperationsClient("MathsTriWShttp");
+            Console.WriteLine(client3.Add(100, 101));
+            Console.WriteLine(client3.Multiply(10, 15));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathsLibrary/MathsOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLibrary
+{
+    public class MathsOperations : IMathsOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                throw new FaultException("Divide by zero forbidden");
+            }
+            return (num1 / num2);
+        }
+    }
+}
